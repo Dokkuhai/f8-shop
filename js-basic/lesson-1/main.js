be7957d44f62961ea7ad5725bd0de2aa2443bd76
Dokkuhai <yoshidadokkuhai@gmail.com>
2024-04-18 21:29:41 +0700

    add code

---
+++ js-basic/lesson-1/main.js
@@ -0,0 +1,28 @@
+/**
+ * Comment in Javascript
+ * 1. Used to:
+ * - Note
+ * - Disable code
+ *
+ * - Comment inline
+ *
+ *
+ *
+ *
+ **/
+
+/*var fullName = "Dokkuhai";
+var age = 20;
+alert(fullName);
+alert(age);*/
+
+//console.log("Day la 1 dong log!");
+//prompt("Hello world");
+
+/*setTimeout(function () {
+  alert("Thong bao");
+}, 1000);*/
+
+setInterval(function () {
+  console.log("hello world" + Math.random());
+}, 1);
